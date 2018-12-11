01d503496a8557fd378f6df3a6ad86ae0cbfef00
zero2smart <fivewisdomzero@gmail.com>
2018-12-11 10:45:59 -0500

    add redux dev tools

--- src/store.js
+++ src/store.js
@@ -1,20 +1,36 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import createSagaMiddleware from 'redux-saga';
-
+import createHistory from 'history/createBrowserHistory';
+import { routerMiddleware } from 'react-router-redux';
 import rootReducer from './reducers/rootReducer';
 import sagas from './sagas/rootSaga';
 
 const sagaMiddleware = createSagaMiddleware();
 
+export const history = createHistory();
+
 const initialState = {};
+const enhancers = [];
 const middleware = [
-    sagaMiddleware
+  routerMiddleware(history),
+  sagaMiddleware
 ];
+
+if (process.env.NODE_ENV === 'development') {
+  const devToolsExtension = window.__REDUX_DEVTOOLS_EXTENSION__;
+
+  if (typeof devToolsExtension === 'function') {
+    enhancers.push(devToolsExtension());
+  }
+}
 
 const store = createStore(
     rootReducer,
     initialState,
-    applyMiddleware(...middleware)
+    compose(
+      applyMiddleware(...middleware),
+      ...enhancers
+    )
 );
 
 sagaMiddleware.run(sagas);
