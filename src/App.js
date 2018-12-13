4dbd2fd6ad5381620a4415820f1aa3705ff75e05
zero2smart <fivewisdomzero@gmail.com>
2018-12-12 19:28:54 -0500

    modify for dashboard

--- src/App.js
+++ src/App.js
@@ -6,25 +6,38 @@ import { Provider } from 'react-redux';
 import { Route, Switch } from 'react-router-dom';
 import store from './store';
 import Dashboard from './components/Dashboard';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/js/bootstrap.js';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faList } from '@fortawesome/free-solid-svg-icons';
 
 class App extends Component {
     render() {
         return (
             <Provider store={store}>
                 <BrowserRouter>
-                    <div className="root">
+                    <div className="root d-flex">
                         <div className="sidebar-wrapper">
 
                         </div>
                         <div className="content-wrapper">
-                            <header>
-                                <FontAwesomeIcon className="bars" color="#E3E3E3" icon={faBars} />
-                                <span>Eagle&nbsp;</span>
-                                <span>/ Dispatch</span>
+                            <header className="row">
+                                <div className="header-left-block col-xl-6 col-md-6 col-sm-6 col-xs-12">
+                                    <FontAwesomeIcon className="fa-my-bars" color="#E3E3E3" icon={faBars} />
+                                    <span>Eagle&nbsp;</span>
+                                    <span>/ Dispatch</span>
+                                </div>
+                                <div className="header-right-block col-xl-6 col-md-6 col-sm-6 col-xs-12 float-sm-right text-sm-right float-md-right text-md-right float-xl-right text-xl-right">
+                                    <ul>
+                                        <li><span>View:</span></li>
+                                        <li>
+                                            <FontAwesomeIcon className="fa-my-list" color="#E3E3E3" icon={faList} />
+                                            <span>Standard</span>
+                                        </li>
+                                        <li>
+                                            <FontAwesomeIcon className="fa-my-list" color="#E3E3E3" icon={faList} />
+                                            <span>Dashboard</span>
+                                        </li>
+                                    </ul>
+                                </div>
                             </header>
                             <main>
                                 <Switch>
